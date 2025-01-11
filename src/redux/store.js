194969c56e9209243d14e1f41633d23b7e35f103
aamrahms <90725468+aamrahms@users.noreply.github.com>
2025-01-11 04:09:53 -0600

    added commands to readme

---
+++ src/redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit';
+import dataReducer from './dataSlice/dataSlice';
+
+export const store = configureStore({
+  reducer: {
+    data: dataReducer,
+  },
+}
+);
