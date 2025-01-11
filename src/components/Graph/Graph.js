617279a68ed767a5989517289c7719edf98290d6
aamrahms <90725468+aamrahms@users.noreply.github.com>
2025-01-11 04:13:54 -0600

    fixed width of graph

--- src/components/Graph/Graph.js
+++ src/components/Graph/Graph.js
@@ -15,7 +15,7 @@ const Graph = () => {
   return (
     <div className="graph">
       <LineChart
-        width={800}
+        width={900}
         height={500}
         data={sales}
         margin={{ top: 20, right: 30, left: 20, bottom: 0 }}
